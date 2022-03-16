4f705005e6c706f5400363a9957f694476741476
skrt-dev <jacobschwantes@outlook.com>
2022-03-16 01:04:34 -0500

    init

---
+++ pages/projects/[projectId]/discussions/[discussionId].tsx
@@ -0,0 +1,5 @@
+import ProjectLayout from '../../../../layouts/ProjectLayout';
+export default function App (props) {
+    return <h1>discussions</h1>
+}
+App.Layout = ProjectLayout;
---
+++ pages/projects/[projectId]/tasks/[taskId].tsx
@@ -0,0 +1,5 @@
+import ProjectLayout from '../../../../layouts/ProjectLayout';
+export default function App (props) {
+    return <h1>tasks</h1>
+}
+App.Layout = ProjectLayout;
